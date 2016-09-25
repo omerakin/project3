e18883fe4f586573b5b54667930fa7f4fe9f964c
Omer Akin <akin__08@hotmail.com>
2016-09-26 01:17:40 +0300

    v1. (some methods filled.)

--- lab3/src/cs601/concurrent/ReentrantReadWriteLock.java
+++ lab3/src/cs601/concurrent/ReentrantReadWriteLock.java
@@ -1,5 +1,8 @@
 package cs601.concurrent;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 /**
  * A reentrant read/write lock that allows: 
@@ -14,11 +17,25 @@ public class ReentrantReadWriteLock {
 	// We should be able to find the number of read locks and the number of write locks 
 	// a thread with the given threadId is holding 
 	
+	private final static Object countLock = new Object();
+	private Map<Thread,Integer> readingThreads;
+	private Map<Thread,Integer> writingThread;
+	private volatile int numberOfReaders;
+	private volatile int numberOfWriters;
+	private volatile int numberOfReadersRequested;
+	private volatile int numberOfWritersRequested;
+	
 	/**
 	 * Constructor for ReentrantReadWriteLock
 	 */
 	public ReentrantReadWriteLock() {
 		// FILL IN CODE
+		readingThreads = new HashMap<Thread,Integer>();
+		writingThread = new HashMap<Thread,Integer>();
+		numberOfReaders = 0;
+		numberOfWriters = 0;
+		numberOfReadersRequested = 0;
+		numberOfWritersRequested = 0;
 	}
 
 	/**
@@ -27,9 +44,9 @@ public class ReentrantReadWriteLock {
 	 * @return
 	 */
 	public synchronized boolean isReadLockHeldByCurrentThread() {
-
 		// FILL IN CODE
-		return false; // don't forget to change it
+		Thread currentthread = Thread.currentThread();
+		return readingThreads.containsKey(currentthread); // don't forget to change it
 	}
 
 	/**
@@ -39,8 +56,8 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized boolean isWriteLockHeldByCurrentThread() {
 		// FILL IN CODE
-		
-		return false; // don't forget to change it
+		Thread currentthread = Thread.currentThread();
+		return writingThread.containsKey(currentthread); // don't forget to change it
 	}
 
 	/**
@@ -51,8 +68,16 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized boolean tryAcquiringReadLock() {
 		// FILL IN CODE
-		
-		return false; // don't forget to change it
+		if (writingThread.containsKey(Thread.currentThread())) {
+			incrementNumberOfReadersRequested();
+			return true;
+		} else if(numberOfWriters > 0 || numberOfWritersRequested > 0) {
+			return false;
+		} else {
+			incrementNumberOfReadersRequested();
+			return true;
+		}
+		// don't forget to change it
 	}
 
 	/**
@@ -63,8 +88,17 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized boolean tryAcquiringWriteLock() {
 		// FILL IN CODE
-		
-		return false; // don't forget to change it
+		if (writingThread.containsKey(Thread.currentThread())) {
+			incrementNumberOfWritersRequested();
+			return true;
+		} else if (numberOfWriters > 0 || numberOfReaders > 0 || 
+				numberOfWritersRequested > 0 || numberOfReadersRequested > 0) {
+			return false;
+		} else {
+			incrementNumberOfWritersRequested();
+			return true;
+		}
+		// don't forget to change it
 	}
 
 	/**
@@ -75,6 +109,31 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized void lockRead() {
 		// FILL IN CODE
+		while(!tryAcquiringReadLock()){
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		Thread currentthread =Thread.currentThread(); 
+		getSetReadingCount(currentthread);
+		decrementNumberOfReadersRequested();
+		incrementNumberOfReadLocks();
+	}
+
+	private Integer getSetReadingCount(Thread currentthread) {
+		// TODO Auto-generated method stub
+		int count;
+		if(readingThreads.containsKey(currentthread)){
+			count = readingThreads.get(currentthread) + 1;
+			readingThreads.remove(currentthread);
+		} else {
+			count = 1;
+		}
+		readingThreads.put(currentthread, count);
+		return null;
 	}
 
 	/**
@@ -82,7 +141,14 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized void unlockRead() {
 		// FILL IN CODE
-		
+		Thread currentthread = Thread.currentThread();
+		if(readingThreads.get(currentthread) == 1){
+			readingThreads.remove(currentthread);
+		} else {
+			readingThreads.put(currentthread, readingThreads.get(currentthread) - 1);
+		}		
+		decrementNumberOfReadLocks();
+		notifyAll();
 	}
 
 	/**
@@ -93,14 +159,89 @@ public class ReentrantReadWriteLock {
 	 */
 	public synchronized void lockWrite() {
 		// FILL IN CODE
-		
+		while(!tryAcquiringWriteLock()) {
+			try {
+				this.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		Thread currentthread = Thread.currentThread();
+		getWritingCount(currentthread);
+		decrementNumberOfWritersRequested();
+		incrementNumberOfWriteLocks();
+	}
+
+	private Integer getWritingCount(Thread currentthread) {
+		// TODO Auto-generated method stub
+		int count;
+		if(writingThread.containsKey(currentthread)){
+			count = writingThread.get(currentthread) + 1;
+			writingThread.remove(currentthread);
+		} else {
+			count = 1;
+		}
+		writingThread.put(currentthread, count);
+		return null;
 	}
 
 	/**
 	 * Releases the write lock held by the current thread. 
 	 */
-
 	public synchronized void unlockWrite() {
 		// FILL IN CODE
+		Thread currentthread = Thread.currentThread();
+		if(writingThread.get(currentthread) == 1){
+			writingThread.remove(currentthread);
+		} else {
+			writingThread.put(currentthread, writingThread.get(currentthread) - 1);
+		}
+		decrementNumberOfWriteLocks();
+		notifyAll();
+	}
+	
+	public void incrementNumberOfReadLocks() {
+		synchronized(countLock) {
+			numberOfReaders++;
+		}
+	}
+	public void decrementNumberOfReadLocks() {
+		synchronized(countLock) {
+			numberOfReaders--;
+		}
+	}
+	
+	public void incrementNumberOfWriteLocks() {
+		synchronized(countLock) {
+			numberOfWriters++;
+		}
+	}
+	public void decrementNumberOfWriteLocks() {
+		synchronized(countLock) {
+			numberOfWriters--;
+		}
+	}
+	
+	public void incrementNumberOfReadersRequested() {
+		synchronized(countLock) {
+			numberOfReadersRequested++;
+		}
+	}
+	public void decrementNumberOfReadersRequested() {
+		synchronized(countLock) {
+			numberOfReadersRequested--;
+		}
+	}
+	
+	public void incrementNumberOfWritersRequested() {
+		synchronized(countLock) {
+			numberOfWritersRequested++;
+		}
+	}
+	public void decrementNumberOfWritersRequested() {
+		synchronized(countLock) {
+			numberOfWritersRequested--;
+		}
 	}
 }

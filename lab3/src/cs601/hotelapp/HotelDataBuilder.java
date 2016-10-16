7ac567d4f8d60743c3e92bcc46bdefc4b40e489b
Jump Thanawut <thanawutanan@hotmail.com>
2016-10-15 17:45:50 -0700

    review

--- lab3/src/cs601/hotelapp/HotelDataBuilder.java
+++ lab3/src/cs601/hotelapp/HotelDataBuilder.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
